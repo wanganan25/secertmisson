6490dc1fbcf9eed6ca214b1c97ae5c613cf1111c
wanganan25 <super2023gpt4@gmail.com>
2025-09-18 16:37:44 +0800

    update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-﻿嚜磨mport { initializeApp } from 'https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js';
 import {
   getFirestore,
   doc,
